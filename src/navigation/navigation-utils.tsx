952d8091351a190a3ce9fd8e0716ff9c3829e64e
PiotrPabich <87279370+PiotrPabichCode@users.noreply.github.com>
2023-10-03 21:18:28 +0200

    static navigationRef

--- src/navigation/navigation-utils.tsx
+++
@@ -1,7 +0,0 @@
-import { useNavigation } from '@react-navigation/native';
-
-const navigation = useNavigation(); // Initialize the navigation object once
-
-export function navigate(name: any, params?: any) {
-  return navigation.navigate(name, params);
-}
---
+++ src/navigation/navigationUtils.tsx
@@ -0,0 +1,20 @@
+import { createNavigationContainerRef } from '@react-navigation/native';
+import { RootStackParamList } from './types';
+
+export const navigationRef = createNavigationContainerRef<RootStackParamList>();
+
+export function navigate(name: string, params?: object) {
+  if (navigationRef.isReady() && navigationRef.current?.getState()) {
+    navigationRef.current.navigate(name as never, params as never);
+  } else {
+    setTimeout(() => {
+      navigate(name as never, params as never);
+    });
+  }
+}
+
+export function goBack() {
+  if (navigationRef.isReady() && navigationRef.canGoBack()) {
+    navigationRef.goBack();
+  }
+}
